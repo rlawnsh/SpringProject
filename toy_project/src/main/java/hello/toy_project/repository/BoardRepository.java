54de9b4251a05765d40abe8600d658acbc2a23e1
wnshrla <wnshrla@naver.com>
2022-02-01 16:37:33 +0900

    JPA를 이용한 페이지 처리 및 검색

--- toy_project/src/main/java/hello/toy_project/repository/BoardRepository.java
+++ toy_project/src/main/java/hello/toy_project/repository/BoardRepository.java
@@ -3,7 +3,12 @@ package hello.toy_project.repository;
 import hello.toy_project.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    List<Board> findByTitle(String title);
+
+    List<Board> findByTitleOrContent(String title, String content);
 }
 
