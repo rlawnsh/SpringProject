c7fd12aafa52483199cddaa0707328dd09609c9e
wnshrla <wnshrla@naver.com>
2022-02-01 15:02:04 +0900

    thymeleaf 에서 form 전송하기

--- toy_project/src/main/java/hello/toy_project/controller/BoardController.java
+++ toy_project/src/main/java/hello/toy_project/controller/BoardController.java
@@ -1,16 +1,53 @@
 package hello.toy_project.controller;
 
+import hello.toy_project.entity.Board;
+import hello.toy_project.repository.BoardRepository;
+import hello.toy_project.validator.BoardValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
-@RequestMapping()
+@RequestMapping("board")
+@RequiredArgsConstructor
 public class BoardController {
 
-    @GetMapping("board/list")
-    public String list() {
+    private final BoardRepository boardRepository;
+    private final BoardValidator boardValidator;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Board> findBoards = boardRepository.findAll();
+        model.addAttribute("findBoards", findBoards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String post(@Valid Board new_board, BindingResult bindingResult) {
+        boardValidator.validate(new_board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+//        System.out.println("new_board = " + new_board);
+        boardRepository.save(new_board);
+        return "redirect:/board/list";
     }
 }
 
